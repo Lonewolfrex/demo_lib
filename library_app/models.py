f3b1cbdafcae18971d3f9b141f50eb6f015f08ea
Lonewolfrex <samparkprakash27@gmail.com>
2025-12-02 15:53:32 +0530

    updated table for donated books

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,4 @@
+# library_app/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,14 +8,31 @@ class Book(models.Model):
     description = models.TextField(blank=True)
     available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
 class Rental(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     rented_on = models.DateTimeField(auto_now_add=True)
     returned_on = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
+
 class Donation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book_title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     donated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.book_title} ({self.user.username})"
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    purchased_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} purchased {self.book.title}"
